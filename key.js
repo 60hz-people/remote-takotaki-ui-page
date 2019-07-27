a542d74ae80ce93bf26faa14ebbd2581333cd4a3
Yoshiki Shino <yoshiki.shino.tech@gmail.com>
2019-07-27 14:30:30 +0900

    [Add] implement WebRTC

---
+++ key.js
@@ -0,0 +1,1 @@
+window.__SKYWAY_KEY__ = '325eb728-51a2-40cc-94bf-fca8bcb5885c';
---
+++ motion.js
@@ -0,0 +1,28 @@
+console.log('モーション処理');
+
+window.addEventListener("deviceorientation", handleOrientation, true);
+
+function handleOrientation(event) {
+  var absolute = event.absolute;
+  var alpha    = event.alpha;
+  var beta     = event.beta;
+  var gamma    = event.gamma;
+
+  // 方向データに基づいて処理を行う
+  //console.log(event);
+}
+
+
+window.addEventListener("devicemotion", handleOrientation, true);
+
+function handleOrientation(event) {
+
+  var x  = parseFloat(event.acceleration.x);
+  var y  = parseFloat(event.acceleration.y);
+  var z  = parseFloat(event.acceleration.z);
+
+  // 加速度データに基づいて処理を行う
+  // 何故かNullになる？
+  //console.log(x + ' ' + y + ' ' + z );
+
+}
